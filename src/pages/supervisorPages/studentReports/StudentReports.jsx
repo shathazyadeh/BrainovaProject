ba4ec5d091b86ac3a85d51dd8e7f4143a826dd96
shathazyadeh <shatha.zyadeh@gmail.com>
2026-04-09 02:56:35 +0200

    feat : Create StudentReports page

--- src/pages/supervisorPages/studentReports/StudentReports.jsx
+++ src/pages/supervisorPages/studentReports/StudentReports.jsx
@@ -1,10 +1,225 @@
 import React from 'react'
 
+import { useNavigate, useParams } from 'react-router-dom';
+import useGetAllOfMyStudentsCases from '../../../hooks/supervisorHooks/useGetAllOfMyStudnetsCases';
+import useDownloadPDF from "../../../hooks/supervisorHooks/useDownloadPDF";
+import { Avatar, Box, Button, Container, Grid, Typography } from '@mui/material';
+import DashboardNavbar from '../../../components/muiComponents/dashboardNavbar/DashboardNavbar';
+import { Link as RouterLink } from "react-router-dom";
+import { IoMdEye } from "react-icons/io";
+import { FaArrowLeft } from "react-icons/fa6";
 function StudentReports() {
+  const {studentId}=useParams();
+  const navigate=useNavigate();
+  const { isError, error, isLoading, data } = useGetAllOfMyStudentsCases(studentId);
+  console.log("dataaaaaa:",data);
+  const total = data?.items?.length || 0;
+const digits = Math.max(3, String(total).length); //لكتابة اي دي التقرير 
+console.log("digit:",digits);// كم اكبر عدد ديجيت ممكن اوصله في كتابة رقم التقرير
   return (
-    <div>
-      
-    </div>
+    <Box
+      sx={{
+        bgcolor: "var(--navy-color)",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+    <DashboardNavbar />
+          <Box
+            component={"section"}
+            sx={{
+              paddingBottom: "50px",
+              flexGrow: 1,
+              alignItems: "flex-start",
+              display: "block",
+              minHeight: "100vh",
+            }}
+          >
+            <Container maxWidth="lg">
+
+              <Box className='student_info'>
+                   <Box
+                  sx={{
+                    display: "flex",
+                    paddingBottom: "20px",
+                    alignItems: "center",
+                  }}
+                >
+
+
+                  <FaArrowLeft color='#fff' size={20} style={{ cursor: "pointer" }} onClick={() => navigate("/dashboard/supervisor/students")}/>
+                  <Avatar
+                    sx={{
+                      marginLeft:'15px',
+                      bgcolor: "var(--primary-color)",
+                      fontSize: "22px",
+                      width: 50,
+                      height: 50,
+                      boxShadow: "0 0 15px rgba(207, 25, 25, 0.51)",
+                    }}
+                  >
+                    {data?.items?.[0]?.studentName?.charAt(0)?.toUpperCase()}
+                  </Avatar>
+                  <Box className="flex_column">
+                    <Typography
+                      sx={{
+                        color: "#fff",
+                        paddingLeft: "10px",
+                        fontFamily: "var(--primary-font)",
+                        fontWeight: "800",
+                        fontSize:'23px',
+                        whiteSpace: { md: "nowrap" }
+                      }}
+                    >
+                      {data?.items?.[0]?.studentName || ""}
+                    </Typography>
+                    
+                  </Box>
+                </Box>
+              </Box>
+
+              <Grid container spacing={2}>
+                {data?.items.map((report,index)=>
+                 <Grid item size={{md:4}}>
+                      <Box className="report" sx={{bgcolor:'#15181e',padding:'25px',border: "1px solid #ff01013b",borderRadius: "15px",}}>
+                        <Box className='report_details'>
+                          <Typography sx={{color:'#fff',fontWeight:'500',fontSize:'15px',marginBottom:'10px',fontFamily: "var(--primary-font)",}}>{`RPT-${String(index + 1).padStart(digits, "0")}`}</Typography>
+                          <Typography sx={{color:'#7e8a9a',fontWeight:'400',fontSize:'13px',fontFamily: "var(--primary-font)",}}>{`CASE-${report?.caseId?.slice(0, 6)}`}</Typography>
+                          <Typography sx={{color:'#7e8a9a',fontWeight:'400',fontSize:'11px',paddingTop:'3px',fontFamily: "var(--primary-font)",}}>{report.reportSubmittedAt.split("T")[0]}</Typography>
+                        </Box>
+                       <Box className="ai_predict_details" sx={{border: "1px solid #57565662",borderRadius: "15px",bgcolor:'#181b21',paddingX:'10px',marginTop:'15px',paddingY:'13px'}}>
+                        <Typography sx={{color:"#7e8a9a",fontWeight:'500',fontSize:'13px',fontFamily: "var(--primary-font)",}}>AI Prediction</Typography>
+                           <Typography  sx={{fontSize: "15px",paddingY: "5px",paddingX: "14px",borderRadius: "15px",display: "inline-flex", marginTop: "10px",
+                               bgcolor:
+                                 report?.predictionResult === "meningioma"
+                                   ? "rgb(55, 44, 28)"
+                                   : report?.predictionResult === "glioma"
+                                   ? "rgb(51, 26, 32)"
+                                   : report?.predictionResult === "notumor"
+                                   ? "rgb(23, 49, 40)"
+                                   : report?.predictionResult === "pituitary"
+                                   ? "#400f4d7c"
+                                   : "#781234",
+                                color:
+                                  report?.predictionResult === "meningioma"
+                                    ? "rgb(218, 148, 14)"
+                                    : report?.predictionResult === "glioma"
+                                    ? "rgb(196, 36, 38)"
+                                    : report?.predictionResult === "notumor"
+                                    ? "rgb(30, 167, 69)"
+                                    : report?.predictionResult === "pituitary"
+                                    ? "#9f05ffab"
+                                    : "var(--primary-color)",
+                                     }}>
+                                ● {report?.predictionResult}
+                            </Typography>
+                       </Box>
+                        <Box className='feedback' sx={{marginTop:'15px',borderBottom:'1px solid #3434348e',paddingBottom:'15px',display:'flex',justifyContent: "space-between",alignItems:'center'}}>
+                          <Typography sx={{color:"#7e8a9a",fontWeight:'400',fontSize:'13px',fontFamily: "var(--primary-font)",}}>Feedback</Typography>
+                          {report.isReviewed? (
+                             <Typography
+                                  sx={{
+                                    width: "fit-content",
+                                    paddingX: "14px",
+                                    paddingY: "5px",
+                                    borderRadius: "20px",
+                                    backgroundColor: "#173128",
+                                    color: "#1FA143",
+                                    fontSize: "13px",
+                                    display: "flex",
+                                    whiteSpace: "nowrap",
+                                  }}
+                                >
+                                  Submitted
+                                </Typography>
+                               ) :(
+                                  <Typography
+                                      sx={{
+                                        width: "fit-content",
+                                        paddingX: "10px",
+                                        paddingY: "5px",
+                                        borderRadius: "20px",
+                                        backgroundColor: "#23272f",
+                                        color: "#718296",
+                                        fontSize: "13px",
+                                        display: "flex",
+                                        whiteSpace: "nowrap",
+                                      }}
+                                    >
+                                      No Feedback
+                                    </Typography>
+                                )}
+                        </Box>
+                        <Box className="view_details">
+                           <Button component={RouterLink} to={`/dashboard/supervisor/report-details/${report.reportId}`}
+                               sx={{
+                                 borderRadius: "15px",
+                                   bgcolor: "#6f6e6e3b",
+                                   color: "#ffffff",
+                                   paddingX: "30px",
+                                   textAlign: "center",
+                                   width: "100%",
+                                   marginTop: "10px",
+                                   fontSize: "12px",
+                                   textTransform: "none",
+                                   display:'flex',
+                                   gap:"10px",
+                                   "&:hover": {
+                                     bgcolor: " #ff00009f",
+                                     boxShadow: "0 0 15px rgba(207, 25, 25, 0.51)",
+                                   },
+                                 }}
+                               >
+                                    <IoMdEye size={20}/>
+                                  View Reports
+                             </Button>
+                        </Box>
+                      </Box>
+                    
+                  </Grid>
+                )}
+                 
+              </Grid>
+
+
+            </Container>
+      </Box>
+
+
+
+
+
+
+{/*footer */}
+  <Box
+        className="lower_footer"
+        sx={{
+          borderTop: "1px solid rgba(53, 53, 53, 0.93)",
+          width: "fit-content",
+          margin: "auto",
+          paddingX: { xs: "0px", md: "200px" },
+          textAlign: "center",
+          marginTop: { xs: "60px", md: "0px" },
+        }}
+      >
+        <Typography
+          component={"p"}
+          sx={{ color: "var(--mid-gray-color)", paddingY: "30px" }}
+        >
+          © 2026{" "}
+          <Typography
+            component={"span"}
+            sx={{ color: "var(--dark-red-color)" }}
+          >
+            Brainova
+          </Typography>
+          . All rights reserved. | Built for medical education and research
+          purposes.
+        </Typography>
+      </Box>
+
+    </Box>
   )
 }
 
