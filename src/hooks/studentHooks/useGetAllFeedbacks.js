087e8859bbbeb6d57eb497c0941ebf65d407e04c
shathazyadeh <shatha.zyadeh@gmail.com>
2026-04-10 23:26:16 +0200

    feat: implement useGetAllFeedbacks hook for retrieving student feedbacks

---
+++ src/hooks/studentHooks/useGetAllFeedbacks.js
@@ -0,0 +1,7 @@
+import useFetch from "../generalHooks/useFetch";
+
+export default function useGetAllFeedbacks() {
+  return useFetch("/Student/Feedbacks", ["studentFeedbacks"], {
+    refetchInterval: false,
+  });
+}
