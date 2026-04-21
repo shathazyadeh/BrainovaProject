0d3b927a59842cb3e8d49b100fec3ec0a0aee65c
shathazyadeh <shatha.zyadeh@gmail.com>
2026-04-21 02:13:39 +0200

    feat: add expandable inner content to questions table

--- src/components/muiComponents/questionsTabel/QuestionsTabel.jsx
+++ src/components/muiComponents/questionsTabel/QuestionsTabel.jsx
@@ -20,11 +20,6 @@ function Row(props) {
   console.log("row ", row);
   const [open, setOpen] = React.useState(false);
   const [checked, setChecked] = React.useState(row?.isActive);
-  console.log(row?.isActive);
-
-  React.useEffect(() => {
-    setChecked(row?.isActive);
-  }, [row?.isActive]);
 
   const handleToggle = (id, value) => {
     setChecked(value);
@@ -32,8 +27,8 @@ function Row(props) {
 
   return (
     <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        <TableCell>
+      <TableRow sx={{ '& > *': { borderBottom: open ? "none" : "1px solid #1e1d1d" } }}>
+        <TableCell align="center" sx={{width: "50px",paddingLeft:"0px"}}>
           <IconButton
             aria-label="expand row"
             size="small"
@@ -140,7 +135,7 @@ function Row(props) {
               alignItems: "center",
               transition: "all .3s",
               "&:hover": {
-                backgroundColor: "rgba(229, 226, 226, 0.21)",
+                bgcolor: "rgba(229, 226, 226, 0.21)",
               },
             }}
           >
@@ -150,26 +145,34 @@ function Row(props) {
       </TableRow>
 
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
           <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-
-                <TableBody></TableBody>
-              </Table>
+          <Box className="inner_content" sx={{color:"#fff", paddingBottom:"30px",paddingTop:"10px",paddingLeft:"70px"}}>
+            <Box className="options" sx={{display:"flex",gap:"8px",marginBottom:"20px"}}>
+                {row?.type===2 ? 
+            row?.options?.map(option=>
+            <Typography sx={{width:"fit-content",bgcolor:"rgba(229, 226, 226, 0.21)",borderRadius: "15px",paddingY:"2px",paddingX:"10px",color:"rgb(209, 206, 206)",fontSize:"12px"}}>
+                {option}
+            </Typography>
+            )
+            : 
+            <Typography sx={{color:"var(--secondary-color)",fontSize:"13px"}}>No options defined.</Typography>
+            }
             </Box>
+            <Box className="footer" sx={{display:"flex",gap:"20px"}}>
+                <Typography sx={{fontSize:"13px",color:"var(--secondary-color)"}}>Status:{" "}
+                    {row?.isActive ? 
+                    <Typography component={'span'} sx={{color:"#38d479",fontSize:"13px",textShadow: "0 0 6px #38d479"}}>Active</Typography>
+                    :
+                    <Typography component={'span'} sx={{fontSize:"13px"}}>Inactive</Typography>
+                    }
+                </Typography>
+                <Box sx={{color: "var(--primary-color)",display: "flex",alignItems: "center",gap: "2px",cursor:"pointer",transition: "all 0.3s ease",borderBottom: "1px solid transparent",'&:hover': {borderBottom: "1px solid var(--primary-color)"}}}>
+                  <MdOutlineEdit size={15} style={{ transform: "translateY(-1px)" }} />
+                  <Typography sx={{ fontSize: "13px" ,textShadow: "0 0 6px var(--primary-color)"}}>Edit inline</Typography>
+                </Box>
+            </Box>
+          </Box>
           </Collapse>
         </TableCell>
       </TableRow>
